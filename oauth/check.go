6fd0542e3f8fc8ea1efa32ae23bfb7b3bf2a7854
sqb550 <3180607552@qq.com>
2026-08-28 19:18:04 +0800

    feat: 增加统一身份认证 OAuth 工具

---
+++ oauth/check.go
@@ -0,0 +1,81 @@
+package oauth
+
+import (
+	"bytes"
+	"net/http"
+	"regexp"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/go-resty/resty/v2"
+	"github.com/zjutjh/mygo/oauth/oauthException"
+)
+
+// GetLoginMsg 获取登陆失败后页面上的提示语
+func GetLoginMsg(resp *resty.Response) string {
+	re := regexp.MustCompile(`<span\s+id="msg">(.+?)</span>`)
+	matches := re.FindStringSubmatch(resp.String())
+	if len(matches) == 0 {
+		return ""
+	}
+	// 删除span内部的标签
+	re = regexp.MustCompile(`<[^>]*>`)
+	msg := re.ReplaceAllString(matches[1], "")
+	return msg
+}
+
+// CheckLogin 用于判断登陆是否成功
+func CheckLogin(resp *resty.Response) error {
+	// 判断登陆是否成功
+	destination := resp.RawResponse.Request.URL.String()
+	if destination == PersonalCenterURL {
+		// 登陆成功后会跳转到用户中心
+		return nil
+	}
+
+	// 判断是否需要修改密码
+	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(resp.Body()))
+	if err != nil {
+		return err
+	}
+	title := doc.Find("title").Text()
+	if title == "修改密码" {
+		return oauthException.EditPasswordError
+	}
+
+	// 判断失败原因
+	msg := GetLoginMsg(resp)
+	switch msg {
+	case WrongPasswordMsg:
+		return oauthException.WrongPassword
+	case WrongAccountMsg:
+		return oauthException.WrongAccount
+	case NotActivatedMsg:
+		return oauthException.NotActivatedError
+	}
+	return oauthException.OtherError
+}
+
+// CheckIsClosed 判断统一是否关闭
+func CheckIsClosed() error {
+	resp, err := http.Get(PersonalCenterURL)
+	if err != nil {
+		return err
+	}
+
+	// 校验httpStatusCode
+	if resp.StatusCode != http.StatusOK {
+		return oauthException.ClosedError
+	}
+
+	// fallback 校验内容
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	title := doc.Find("title").Text()
+	if title == "Error 403.6" || title == "Error 403" {
+		return oauthException.ClosedError
+	}
+	return nil
+}
---
+++ oauth/oauthException/exception.go
@@ -0,0 +1,28 @@
+package oauthException
+
+// 用于维护一些 `统一验证` 相关的 errCode
+var (
+	WrongAccount      = newError(501, "账号错误")
+	WrongPassword     = newError(502, "密码错误")
+	ClosedError       = newError(503, "统一系统当前不可用")
+	EditPasswordError = newError(503, "统一密码需要修改, 请手动登录统一修改")
+	NotActivatedError = newError(504, "账号未激活")
+	OtherError        = newError(599, "其他错误")
+)
+
+// Error 自定义错误
+type Error struct {
+	Code int
+	Msg  string
+}
+
+func (e Error) Error() string {
+	return e.Msg
+}
+
+func newError(code int, msg string) *Error {
+	return &Error{
+		Code: code,
+		Msg:  msg,
+	}
+}
